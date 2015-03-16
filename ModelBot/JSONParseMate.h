e6cdc2175c159261df3945156f1fb231a2a1b4b2
welsonla <wyc.jar@gmail.com>
2015-03-16 18:24:36 +0800

    可以解析并组装模板内容

--- ModelBot/JSONParseMate.h
+++ ModelBot/JSONParseMate.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <runtime/runtime.h>
+#import <objc/runtime.h>
 
 @interface JSONParseMate : NSObject
 
--- ModelBot/JSONParseMate.m
+++ ModelBot/JSONParseMate.m
@@ -38,14 +38,36 @@
     {
         NSString *className = NSStringFromClass([jsonValues[i] class]);
         NSString *classProperty = modelKeys[i];
+        NSString *classValue = [jsonValues[i] stringValue];
         
+        //坑爹的判断，为什么都是NSCFString
+        if ([jsonValues[i] isKindOfClass:[NSNumber class]])
+        {
+            properties = [properties stringByAppendingFormat:@"@property (nonatomic, assign) NSInteger %@;\n",classProperty];
+        }
+        else if([jsonValues[i] isKindOfClass:[NSString class]])
+        {
+            properties = [properties stringByAppendingFormat:@"@property (nonatomic, strong) NSString *%@;\n",classProperty];
+        }
+        else if([jsonValues[i] isKindOfClass:[NSArray class]])
+        {
+             properties = [properties stringByAppendingFormat:@"@property (nonatomic, strong) NSArray *%@;\n",classProperty];
+        }
         
-        NSLog(@"className:%s--value:%@",object_getClassName(jsonValues[i]),jsonValues[i]);
-        BOOL isMetaClass = 
-        properties = [properties stringByAppendingFormat:@"@property (nonatomic, strong) %@ *%@;\n",className,classProperty];
-        NSLog(@"properties:%@",properties);
+        NSLog(@"class:%@--%@----%s",[jsonValues[i] description],NSStringFromClass([classValue class]),object_getClassName(classValue));
     }
     
+    //获取模板地址
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Template_NSObject" ofType:@""];
+    
+    //读取模板的内容
+    NSString *templateText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    
+    //拼装模板
+    NSString *context = [NSString stringWithFormat:templateText,@"ssss",properties];
+    
+    
+    NSLog(@"properties:%@ --- %@ --- %@",properties,templateText,context);
 }
 
 @end
